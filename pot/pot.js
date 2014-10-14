d7600ec12c66a1fc4af257895c1f39d9aab2f786
Joe Chasinga <jo.chasinga@gmail.com>
2014-10-14 11:43:34 -0400

    Replace Number.map with Fn.map

--- pot/pot.js
+++ pot/pot.js
@@ -1,9 +1,10 @@
 var Firebase = require("firebase");
 var five = require("johnny-five");
 
-// Create a new instance of Firebase db
+// Create a new reference of Firebase db
 var firebaseRef = new Firebase(
-  "https://burning-limbo-666.firebaseio.com/colors"
+  // fictional URL, replace it with your own from Firebase
+  "https://burning-limbo-6660.firebaseio.com/colors"
 );
 
 five.Board().on("ready", function() {
@@ -20,35 +21,33 @@ five.Board().on("ready", function() {
   // Create a new led array based on pin number
   var ledArray = new five.Led.Array([9, 10, 11]);
 
-  // analogRead on pin A0
+  // Listen on data change
   pot.on("data", function() {
 
     var self = this.value;
+    // Print pot value 
     console.log(self);
 
-    // Implement new map method
-    Number.prototype.map = function(fromLow , fromHigh , toLow , toHigh) {
-      return (this - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
-    }
-
-    
     // Map dynamic color brightness to pot value
-    // RED - VIOLET - BLUE
-    var redDec   = Math.round(self.map(offset, offset*2, 255, 0));
-    var blueInc  = Math.round(self.map(0, offset, 0, 255));
+    // RED - MAGENTA - BLUE
+    var redDec   = five.Fn.map(self, offset, offset*2, 255, 0);
+    var blueInc  = five.Fn.map(self, 0, offset, 0, 255);
     // BLUE - CYAN - GREEN
-    var blueDec  = Math.round(self.map(offset*4, offset*5, 255, 0));
-    var greenInc = Math.round(self.map(offset*2, offset*3, 0, 255));
+    var blueDec  = five.Fn.map(self, offset*4, offset*5, 255, 0);
+    var greenInc = five.Fn.map(self, offset*2, offset*3, 0, 255);
     // GREEN - YELLOW - RED
-    var greenDec = Math.round(self.map(offset*5, offset*6, 255, 0));
-    var redInc   = Math.round(self.map(offset*4, offset*5, 0, 255));
+    var greenDec = five.Fn.map(self, offset*5, offset*6, 255, 0);
+    var redInc   = five.Fn.map(self, offset*4, offset*5, 0, 255);
 
+    // Adjusting color brightness conditionally based on 
+    // the location of the pot output value.
     switch (true) {
       case (self >= 0 && self <= offset):
         console.log("1st loop");
         ledArray[0].brightness(255);
         ledArray[2].brightness(blueInc);
         ledArray[1].brightness(0);
+	// update firebase colors' child node r, g, b
 	firebaseRef.set({"r": 255, "b": blueInc, "g": 0});
         break;
       case (self > offset && self <= offset*2):
@@ -56,6 +55,7 @@ five.Board().on("ready", function() {
         ledArray[0].brightness(redDec);
         ledArray[2].brightness(255);
         ledArray[1].brightness(0);
+	// update firebase colors' child node r, g, b
 	firebaseRef.set({"r": redDec, "b": 255, "g": 0});
 	break;
       case (self > offset*2 && self <= offset*3):
@@ -63,6 +63,7 @@ five.Board().on("ready", function() {
         ledArray[0].brightness(0);
         ledArray[2].brightness(255);
         ledArray[1].brightness(greenInc);
+	// update firebase colors' child node r, g, b
 	firebaseRef.set({"r": 0, "b": 255, "g": greenInc});
 	break;
       case (self > offset*3 && self <= offset*4):
@@ -70,6 +71,7 @@ five.Board().on("ready", function() {
         ledArray[0].brightness(0);
         ledArray[2].brightness(blueDec);
         ledArray[1].brightness(255);
+	// update firebase colors' child node r, g, b
 	firebaseRef.set({"r": 0, "b": blueInc, "g": 0});
         break;
       case (self > offset*4 && self <= offset*5):
@@ -77,6 +79,7 @@ five.Board().on("ready", function() {
         ledArray[0].brightness(redInc);
         ledArray[2].brightness(0);
         ledArray[1].brightness(255);
+	// update firebase colors' child node r, g, b
 	firebaseRef.set({"r": redInc, "b": 0, "g": 255});
 	break;
       case (self > offset*5 && self <= offset*6):
@@ -84,6 +87,7 @@ five.Board().on("ready", function() {
         ledArray[0].brightness(255);
         ledArray[2].brightness(0);
         ledArray[1].brightness(greenDec);
+	// update firebase colors' child node r, g, b
 	firebaseRef.set({"r": 255, "b": 0, "g": greenDec});
 	break;
       default:
@@ -91,6 +95,7 @@ five.Board().on("ready", function() {
         ledArray[0].brightness(255);
 	ledArray[2].brightness(0);
 	ledArray[1].brightness(0);
+	// update firebase colors' child node r, g, b
 	firebaseRef.set({"r": 255, "b": 0, "g": 0});
     }
   });
